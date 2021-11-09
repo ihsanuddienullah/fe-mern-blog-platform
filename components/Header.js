dce8a1bb90443bbb9a0c44a8ce94d6226cf7e59a
ihsanuddienullah <ihsanuddienullah@gmail.com>
2021-11-09 20:03:36 +0700

    using nprogress

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import Link from "next/link";
+import Router from "next/router";
+import NProgress from "nprogress";
+import { APP_NAME } from "../config";
 import { signout, isAuth } from "../actions/auth";
-import { NProgress } from "nprogress";
 import {
     Collapse,
     Navbar,
@@ -13,32 +16,28 @@ import {
     DropdownToggle,
     DropdownMenu,
     DropdownItem,
-    NavbarText,
 } from "reactstrap";
-import { APP_NAME } from "../config";
-import Link from "next/link";
-import Router from "next/router";
 
-Router.onRouteChangeStart = url => NProgress.start();
-Router.onRouteChangeComplete = url => NProgress.done();
-Router.onRouteChangeError = url => NProgress.done();
+Router.onRouteChangeStart = (url) => NProgress.start();
+Router.onRouteChangeComplete = (url) => NProgress.done();
+Router.onRouteChangeError = (url) => NProgress.done();
 
-const Header = (props) => {
+const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => setIsOpen(!isOpen);
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <div>
             <Navbar color="light" light expand="md">
                 <Link href="/">
-                    <NavLink className="font-weight-bold" href="/">
-                        {APP_NAME}
-                    </NavLink>
+                    <NavLink className="font-weight-bold">{APP_NAME}</NavLink>
                 </Link>
                 <NavbarToggler onClick={toggle} />
                 <Collapse isOpen={isOpen} navbar>
-                    <Nav className="mr-auto" navbar>
+                    <Nav className="ml-auto" navbar>
                         {!isAuth() && (
                             <>
                                 <NavItem>
@@ -53,38 +52,40 @@ const Header = (props) => {
                                 </NavItem>
                             </>
                         )}
+
+                        {isAuth() && isAuth().role === 0 && (
+                            <NavItem>
+                                <Link href="/user">
+                                    <NavLink>{`${
+                                        isAuth().name
+                                    }'s Dashboard`}</NavLink>
+                                </Link>
+                            </NavItem>
+                        )}
+
+                        {isAuth() && isAuth().role === 1 && (
+                            <NavItem>
+                                <Link href="/admin">
+                                    <NavLink>{`${
+                                        isAuth().name
+                                    }'s Dashboard`}</NavLink>
+                                </Link>
+                            </NavItem>
+                        )}
+
                         {isAuth() && (
                             <NavItem>
                                 <NavLink
                                     style={{ cursor: "pointer" }}
                                     onClick={() =>
-                                        signout(() => Router.replace("/signin"))
+                                        signout(() => Router.replace(`/signin`))
                                     }
                                 >
                                     Signout
                                 </NavLink>
                             </NavItem>
                         )}
-                        {isAuth() && isAuth().role === 0 && (
-                            <NavItem>
-                                <Link href="/user">
-                                    <NavLink>
-                                        {`${isAuth().name}'s Dashboard'`}
-                                    </NavLink>
-                                </Link>
-                            </NavItem>
-                        )}
-                        {isAuth() && isAuth().role === 1 && (
-                            <NavItem>
-                                <Link href="/admin">
-                                    <NavLink>
-                                        {`${isAuth().name}'s Dashboard'`}
-                                    </NavLink>
-                                </Link>
-                            </NavItem>
-                        )}
                     </Nav>
-                    <NavbarText>Simple Text</NavbarText>
                 </Collapse>
             </Navbar>
         </div>
--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
                     />
                     <link
                         rel="stylesheet"
-                        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"                       
+                        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
                     />
                 </Head>
                 <body>
