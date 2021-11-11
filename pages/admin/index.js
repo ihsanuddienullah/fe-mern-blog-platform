e5dda823a84f91ae224d89ffe90921fadf56674f
ihsanuddienullah <ihsanuddienullah@gmail.com>
2021-11-11 13:29:05 +0700

    60. Category read delete and messages

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -6,7 +6,23 @@ const AdminIndex = () => {
     return (
         <Layout>
             <Admin>
-                <h1>Admin Dashboard</h1>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12 pt-5 pb-5">
+                            <h1>Admin Dashboard</h1>
+                        </div>
+                        <div className="col-md-4">
+                            <ul className="list-group">
+                                <li className="list-group-item">
+                                    <Link href="/admin/crud/category-tag">
+                                        <a>Create Category</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-md-8">right</div>
+                    </div>
+                </div>
             </Admin>
         </Layout>
     );
