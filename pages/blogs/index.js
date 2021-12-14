ae777012c25c25a12a1c944a9795892c19a266cb
ihsanuddienullah <ihsanuddienullah@gmail.com>
2021-12-15 06:02:47 +0700

    89. SEO head section for blogs

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,11 +1,51 @@
 import Head from "next/head";
 import Link from "next/link";
+import { withRouter } from "next/router";
 import Layout from "../../components/Layout";
 import { useState } from "react";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
 import Card from "../../components/blog/card";
+import { APP_NAME, API, DOMAIN, FB_APP_ID } from "../../config";
 
-const Blogs = ({ blogs, categories, tags, size }) => {    
+const Blogs = ({ blogs, categories, tags, size, router }) => {
+    const head = () => {
+        return (
+            <Head>
+                <title>Programming blogs | {APP_NAME}</title>
+                <meta
+                    name="description"
+                    content="Programming blogs and tutorials on react node next vue php laravel and web development"
+                />
+                <link rel="canonical" href={`${DOMAIN}${router.pathname}`} />
+                <meta
+                    property="og:title"
+                    content={`Latest web development tutorials | ${APP_NAME}`}
+                />
+                <meta
+                    property="og:description"
+                    content="Programming blogs and tutorials on react node next vue php laravel and web development"
+                />
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:url"
+                    content={`${DOMAIN}${router.pathname}`}
+                />
+                <meta property="og:site_name" content={`${APP_NAME}`} />
+
+                <meta
+                    property="og:image"
+                    content={`${DOMAIN}/static/images/seoblog.png`}
+                />
+                <meta
+                    property="og:image:secure_url"
+                    content={`${DOMAIN}/static/images/seoblog.png`}
+                />
+                <meta property="og:image:type" content="image/jpg" />
+                <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+            </Head>
+        );
+    };
+
     const showAllBlogs = () => {
         return blogs.map((blog, i) => {
             return (
@@ -40,31 +80,34 @@ const Blogs = ({ blogs, categories, tags, size }) => {
     };
 
     return (
-        <Layout>
-            <main>
-                <div className="container-fluid">
-                    <header>
-                        <div className="col-md-12 pt-3">
-                            <h1 className="display-4 font-weight-bold text-center">
-                                Programming blogs and tutorials
-                            </h1>
-                        </div>
-                        <section>
-                            <div className="pb-5 text-center">
-                                {showAllCategories()}
-                                <br />
-                                {showAllTags()}
+        <>
+            {head()}
+            <Layout>
+                <main>
+                    <div className="container-fluid">
+                        <header>
+                            <div className="col-md-12 pt-3">
+                                <h1 className="display-4 font-weight-bold text-center">
+                                    Programming blogs and tutorials
+                                </h1>
                             </div>
-                        </section>
-                    </header>
-                </div>
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-12">{showAllBlogs()}</div>
+                            <section>
+                                <div className="pb-5 text-center">
+                                    {showAllCategories()}
+                                    <br />
+                                    {showAllTags()}
+                                </div>
+                            </section>
+                        </header>
                     </div>
-                </div>
-            </main>
-        </Layout>
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-md-12">{showAllBlogs()}</div>
+                        </div>
+                    </div>
+                </main>
+            </Layout>
+        </>
     );
 };
 
@@ -83,4 +126,4 @@ Blogs.getInitialProps = () => {
     });
 };
 
-export default Blogs;
+export default withRouter(Blogs);
