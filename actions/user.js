a5abd07e3b54834e54e6b1fb6c4e6e32b3ef0ca3
Muhammad Ihsanuddienullah <Muhammad.Ihsanuddienullah@xapiens.id>
2022-01-06 09:37:10 +0700

    131. Handling token expiry

--- actions/user.js
+++ actions/user.js
@@ -1,5 +1,6 @@
 import fetch from "isomorphic-fetch";
 import { API } from "../config";
+import { handleResponse } from "./auth";
 
 export const userPublicProfile = (username) => {
     return fetch(`${API}/user/${username}`, {
@@ -35,9 +36,10 @@ export const update = (token, user) => {
             Accept: "application/json",
             Authorization: `Bearer ${token}`,
         },
-        body: user
+        body: user,
     })
         .then((response) => {
+            handleResponse(response);
             return response.json();
         })
         .catch((err) => console.log(err));
