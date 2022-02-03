6168550a1f8bf71309e29f5710730418c332c018
Muhammad Ihsanuddienullah <Muhammad.Ihsanuddienullah@xapiens.id>
2022-02-03 22:46:28 +0700

    fix env

---
+++ next.config.production.js
@@ -0,0 +1,14 @@
+module.exports = {
+    publicRuntimeConfig: {
+        APP_NAME: "SEOBLOG-BYIHSAN",
+        API_DEVELOPMENT: "http://localhost:8000/api",
+        API_PRODUCTION: "https://be-mern-blog-platform.herokuapp.com/api",
+        PRODUCTION: true,
+        DOMAIN_DEVELOPMENT: "http://localhost:3000",
+        DOMAIN_PRODUCTION: "https://seoblog-byihsan.vercel.app",
+        FB_APP_ID: "449610619875787",
+        DISQUS_SHORTNAME: "seoblog-wycgccbqew",
+        GOOGLE_CLIENT_ID:
+            "264223307140-0ifgcd452u3lj5bm3jl713vf1hmdi7lc.apps.googleusercontent.com",
+    },
+};
